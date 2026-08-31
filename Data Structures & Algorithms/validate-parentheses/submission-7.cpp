28e5a31a2b80c75dc058afdd6459e113dc3ada18
Fardin Mahid <99063793+Freddydeddydoo@users.noreply.github.com>
2026-08-30 22:18:46 -0600

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string s) {
+
+        vector<char> stk;
+
+        for(int i = 0; i < s.length(); i++){
+            cout << s[i] << endl;
+            if(s[i] == '{' || s[i] == '[' || s[i] == '('){
+                stk.push_back(s[i]);
+            }else if(s[i] == '}' || s[i] == ')' || s[i] == ']'){
+                if(stk.empty()) return false;
+                if(stk.back() == '{' && s[i] != '}' ) return false ;
+                else if(stk.back() == '[' && s[i] != ']') return false;
+                else if(stk.back() == '(' && s[i] != ')') return false;
+                else stk.pop_back();
+            }
+        }
+        return stk.empty();
+    }
+};
